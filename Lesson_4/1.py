b5f1da4e7185981d6002379414294833272d519d
Serega <abissovs@gmail.com>
2019-08-12 01:40:58 +0300

    Lesson 4 homework is done

--- Lesson_4/1.py
+++ Lesson_4/1.py
@@ -3,3 +3,104 @@
 разработанных в рамках домашнего задания первых трех уроков.
 Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 """
+import random
+import cProfile
+from timeit import timeit
+
+
+"""
+L2-2 Посчитать четные и нечетные цифры введенного натурального числа.
+Сравнение рекурсии и цикла (сложность O(n))
+Скорость:
+INPUT: 
+Size: 10    Number: 10000
+OUTPUT:
+Loop:       0.32970349999999904
+Recursion:  0.4401785999999994
+
+INPUT: 
+Size: 100    Number: 10000
+OUTPUT:
+Loop:       4.3700761
+Recursion:  6.312509
+
+Вывод: вариант с использованием цикла более эффективный по времени выполнения
+"""
+
+
+def l2_2_recursion(value):
+
+    def shift(v, i=1, o=0, e=0):
+
+        temp = v % (10 ** i) // (10 ** (i-1))
+
+        if temp % 2 == 0:
+            e += 1
+        else:
+            o += 1
+
+        if v // (10 ** i) == 0:
+            return o, e
+        return shift(v, i + 1, o, e)
+
+    odd_count, even_count = shift(value)
+    return even_count, odd_count
+
+
+def l2_2_loop(value):
+
+    odd_count = 0
+    even_count = 0
+
+    # region i = len(value)
+    i = 0
+    while True:
+        i += 1
+        if value // (10 ** i) == 0:
+            break
+    # endregion
+
+    i -= 1
+    r = 0
+    while i >= 0:
+        value -= r
+        temp = value // (10 ** i)
+        r = temp * (10 ** i)
+
+        if temp % 2 == 0:
+            even_count += 1
+        else:
+            odd_count += 1
+        i -= 1
+
+    return even_count, odd_count
+
+
+def tests():
+
+    size = int(input('Size: '))
+    value = int(''.join([str(random.randint(0, 10)) for i in range(size)]))
+    print(value)
+
+    print('Loop')
+    cProfile.run(f'l2_2_loop({value})')
+    print('='*50)
+    print('Recursion')
+    cProfile.run(f'l2_2_recursion({value})')
+    print('=' * 50)
+
+    count = int(input('Number: '))
+    print('Loop')
+    print(timeit(f'l2_2_loop({value})', setup='from __main__ import l2_2_loop', number=count))
+    print('Recursion')
+    print(timeit(f'l2_2_recursion({value})', setup='from __main__ import l2_2_recursion', number=count))
+
+# ===========================================
+
+
+def main():
+    tests()
+
+
+if __name__ == '__main__':
+    main()
--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -3,3 +3,139 @@
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
+
+from timeit import timeit
+
+
+def sieve_eratosthenes_exm(n):
+    a = [0] * n  # создание массива с n количеством элементов
+    for i in range(n):  # заполнение массива ...
+        a[i] = i  # значениями от 0 до n-1
+
+    # вторым элементом является единица, которую не считают простым числом
+    # забиваем ее нулем.
+    a[1] = 0
+
+    m = 2  # замена на 0 начинается с 3-го элемента (первые два уже нули)
+    while m < n:  # перебор всех элементов до заданного числа
+        if a[m] != 0:  # если он не равен нулю, то
+            j = m * 2  # увеличить в два раза (текущий элемент - простое число)
+            while j < n:
+                a[j] = 0  # заменить на 0
+                j = j + m  # перейти в позицию на m больше
+        m += 1
+
+    # вывод простых чисел на экран (может быть реализован как угодно)
+    b = []
+    for i in a:
+        if a[i] != 0:
+            b.append(a[i])
+
+    del a
+    return b
+
+
+def sieve_eratosthenes(n):
+
+    a = list(range(n))
+    a[1] = 0
+
+    for m in a[2:]:
+        if a[m] == 0:
+            continue
+
+        for j in range(m*2, n, m):
+            a[j] = 0
+
+    r = []
+    for i in a:
+        if i == 0:
+            continue
+        r.append(i)
+    return r
+
+
+def brute_force(n):
+
+    num_array = list(range(2, n))
+    result = []
+
+    for i in num_array:
+        if i in result:
+            continue
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            result.append(i)
+    return result
+
+
+def time_test(n):
+
+    count = int(input('Number: '))
+    methods = ['brute_force', 'sieve_eratosthenes_exm', 'sieve_eratosthenes']
+    cl = len(max(methods))
+    for m in methods:
+        time = timeit(f'{m}({n})', setup=f'from __main__ import {m}', number=count)
+        print(f'{m.capitalize():<{cl}}: {time}')
+
+
+def main():
+
+    n = int(input('Enter N: '))
+    print('Select method')
+    print('Test time - T, brute_force - B, sieve_eratosthenes - E, sieve_eratosthenes_exm - EX')
+    m = input('Method: ').upper()
+
+    if m == 'T':
+        time_test(n)
+        return
+    elif m == 'E':
+        result = sieve_eratosthenes(n)
+    elif m == 'EX':
+        result = sieve_eratosthenes_exm(n)
+    elif m == 'B':
+        result = brute_force(n)
+    else:
+        return
+
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
+    pass
+
+
+'''
+Сложность:
+brute_force - O(n^2)
+sieve_eratosthenes - O(n*log(log n))
+sieve_eratosthenes_exm - O(n*log(log n))
+
+
+Method: Test time
+---------------------------------------------------
+INPUT:
+Enter N: 10      Number: 500
+OUTPUT:
+Brute_force           : 0.006414900000000223
+Sieve_eratosthenes_exm: 0.009184399999999648
+Sieve_eratosthenes    : 0.00630430000000004
+---------------------------------------------------
+INPUT:
+Enter N: 100     Number: 500
+OUTPUT:
+Brute_force           : 0.14554310000000026
+Sieve_eratosthenes_exm: 0.07847960000000054
+Sieve_eratosthenes    : 0.047155499999999684
+---------------------------------------------------
+INPUT:
+Enter N: 1000    Number: 500
+OUTPUT:
+Brute_force           : 8.810421000000002
+Sieve_eratosthenes_exm: 0.9454638000000024
+Sieve_eratosthenes    : 0.5375639999999997
+---------------------------------------------------
+'''
